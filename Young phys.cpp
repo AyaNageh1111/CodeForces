a8cebf727843532b3cb3ce5fdec68760667b604c
AyaNageh1111 <75093490+AyaNageh1111@users.noreply.github.com>
2021-09-24 20:46:58 +0200

    Add files via upload

---
+++ Young phys.cpp
@@ -0,0 +1,24 @@
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	int n;
+//	cin >> n;
+//	int number;
+//	int xsum, ysum, zsum;
+//	for (int i = 0; i < n; i++)
+//	{
+//		
+//			cin >> number;
+//			xsum = count + xsum;
+//		
+//	}
+//	if (count == 0)
+//	{
+//		cout << "YES";
+//	}
+//	else
+//	{
+//		cout << "NO";
+//	}
+//}
